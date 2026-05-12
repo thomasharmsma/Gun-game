0c4c991cf32681a5f07c75477ebabc62dd49d747
ThoompieH <Thomas@harmsma.nl>
2026-05-12 08:24:02 +0200

    removed duplicate code

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -28,18 +28,6 @@ export class Game extends Phaser.Scene {
 
     update() 
     {
-        let lastSide = null;
-
-        function update() {
-            const mouseX = this.input.activePointer.x;
-            const screenWidth = this.scale.width;
-
-            const currentSide = mouseX < screenWidth / 2 ? "left" : "right";
-
-            if (currentSide !== lastSide) {
-                console.log(`Mouse moved to the ${currentSide} side`);
-                lastSide = currentSide;
-            }
-        }
+        
     }
 }
