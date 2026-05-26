1389fb26371e7eaef7721e5fbdbe2df450380d5d
stefanharmsma93 <stefanharmsma93@gmail.com>
2026-05-26 10:24:27 +0200

    playermovement

--- src/gameObjects/player.js
+++ src/gameObjects/player.js
@@ -9,6 +9,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite
 
         this.setCollideWorldBounds(true);
         this.currentSide = 'right';
+        this.playerVelocity = 100;
         this.initAnimations();
     }
 
@@ -67,17 +68,23 @@ export class Player extends Phaser.Physics.Arcade.Sprite
         let isMoving = false;
 
         if (keys.A.isDown) {
+                this.setVelocityX(-this.playerVelocity);
             isMoving = true;
         } else if (keys.D.isDown) {
+                this.setVelocityX(this.playerVelocity);
             isMoving = true;
         } else {
+                this.setVelocityX(0);
         }
 
         if (keys.W.isDown) {
+                this.setVelocityY(-this.playerVelocity);
             isMoving = true;
         } else if (keys.S.isDown) {
+                this.setVelocityY(this.playerVelocity);
             isMoving = true;
         } else {
+            this.setVelocityY(0);
         }
 
         if (!isMoving) {
