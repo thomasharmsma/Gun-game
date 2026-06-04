cf2fe09ca9b6a66eee2af58eb5ee5a5832bc4f7a
thomasharmsma <50795345+thomasharmsma@users.noreply.github.com>
2026-06-04 11:50:17 +0200

    map generation start

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,9 +4,76 @@ document.body.style.cursor = 'none';
 export class Game extends Phaser.Scene {
     constructor() {
         super('Game');
+
+        // map generation
+        this.snake = [];
+        this.snake2 = [];
+        this.food = {};
+        this.direction = 'right';
+        this.nextDirection = 'right';
+        this.direction2 = 'left';
+        this.nextDirection2 = 'left';
+        this.gridSize = 16;
+        this.snakeSpeed = 1;
+        this.lastMoveTime = 0;
+        this.isGameOver = false;
+        this.snakesMoving = true;
     }
 
     create() {
+        this.isGameOver = false;
+        this.snakesMoving = true;
+        this.direction = 'right';
+        this.nextDirection = 'right';
+        this.cameras.main.setBackgroundColor(0x00ff00);
+        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        // Initialize snake
+        this.snake = [];
+        const startX = 25;
+        const startY = 20;
+
+        // Create initial snake body (1 segment)
+        for (let i = 0; i < 1; i++) {
+            const segment = this.add.rectangle(
+                (startX - i) * this.gridSize,
+                startY * this.gridSize,
+                this.gridSize - 2,
+                this.gridSize - 2,
+                0x000000
+            );
+            this.snake.push(segment);
+        }
+
+        // Initialize second snake
+        this.snake2 = [];
+        const startX2 = 25;
+        const startY2 = 20;
+        for (let i = 0; i < 1; i++) {
+            const segment2 = this.add.rectangle(
+                (startX2 + i) * this.gridSize,
+                startY2 * this.gridSize,
+                this.gridSize - 2,
+                this.gridSize - 2,
+                0xff0000
+            );
+            this.snake2.push(segment2);
+        }
+
+        this.time.addEvent({
+            delay: this.snakeSpeed,
+            callback: this.chooseRandomDirection,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.time.addEvent({
+            delay: this.snakeSpeed,
+            callback: this.chooseRandomDirection2,
+            callbackScope: this,
+            loop: true
+        });
+
         this.player = new Player(this, this.scale.width / 2, this.scale.height / 2);
         this.cameras.main.startFollow(this.player);
         this.player.currentSide = 'right';
@@ -65,8 +132,22 @@ export class Game extends Phaser.Scene {
         this.keys = this.input.keyboard.addKeys('W,A,S,D');
     }
 
-    update() 
+    update(time) 
     {
+        if (this.isGameOver) return;
+
+        const totalTilesCovered = this.snake.length + this.snake2.length;
+        if (totalTilesCovered >= 200) {
+            this.snakesMoving = false;
+        }
+
+        // Move snake at fixed intervals
+        if (this.snakesMoving && time >= this.lastMoveTime + this.snakeSpeed) {
+            this.moveSnake();
+            this.moveSnake2();
+            this.lastMoveTime = time;
+        }
+
         this.player.update(this.keys);
         
         const pointer = this.input.activePointer;
@@ -83,5 +164,103 @@ export class Game extends Phaser.Scene {
         this.gun.setFlipY(isMouseLeft);
         this.gun.setPosition(targetX, targetY);
         this.gun.rotation = Phaser.Math.Angle.RotateTo(this.gun.rotation, aimAngle, 0.35);
+    }
+
+    chooseRandomDirection() {
+        const options = ['none'];
+
+        if (this.direction === 'left' || this.direction === 'right') {
+            options.push('up', 'down');
+        } else {
+            options.push('left', 'right');
+        }
+
+        const choice = Phaser.Math.RND.pick(options);
+        if (choice !== 'none') {
+            this.nextDirection = choice;
+        }
+    }
+
+    chooseRandomDirection2() {
+        const options = ['none'];
+
+        if (this.direction2 === 'left' || this.direction2 === 'right') {
+            options.push('up', 'down');
+        } else {
+            options.push('left', 'right');
+        }
+
+        const choice = Phaser.Math.RND.pick(options);
+        if (choice !== 'none') {
+            this.nextDirection2 = choice;
+        }
+    }
+
+    moveSnake() {
+        // Update current direction
+        this.direction = this.nextDirection;
+
+        // Calculate new head position
+        const head = this.snake[0];
+        let newX = head.x;
+        let newY = head.y;
+
+        switch (this.direction) {
+            case 'left':
+                newX -= this.gridSize;
+                break;
+            case 'right':
+                newX += this.gridSize;
+                break;
+            case 'up':
+                newY -= this.gridSize;
+                break;
+            case 'down':
+                newY += this.gridSize;
+                break;
+        }
+
+        // Check for food collision
+        const eating = newX === this.food.x && newY === this.food.y;
+
+        // Move snake
+        const newHead = this.add.rectangle(newX, newY, this.gridSize - 2, this.gridSize - 2, 0x000000);
+        this.snake.unshift(newHead);
+
+        if (!eating) {
+            // Remove tail if not eating
+            // const tail = this.snake.pop();
+            // tail.destroy();
+        } else {
+            // Spawn new food if eating
+            this.food.destroy();
+            this.spawnFood();
+        }
+    }
+
+    moveSnake2() {
+        this.direction2 = this.nextDirection2;
+
+        const head = this.snake2[0];
+        let newX = head.x;
+        let newY = head.y;
+
+        switch (this.direction2) {
+            case 'left':
+                newX -= this.gridSize;
+                break;
+            case 'right':
+                newX += this.gridSize;
+                break;
+            case 'up':
+                newY -= this.gridSize;
+                break;
+            case 'down':
+                newY += this.gridSize;
+                break;
+        }
+
+        const newHead = this.add.rectangle(newX, newY, this.gridSize - 2, this.gridSize - 2, 0xff0000);
+        this.snake2.unshift(newHead);
     }
 }
