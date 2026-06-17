f61950287fecd83ed5eaa273161cf93b8e31b263
stefanharmsma93 <stefanharmsma93@gmail.com>
2026-06-17 10:48:17 +0200

    le commit

--- src/gameObjects/enemy.js
+++ src/gameObjects/enemy.js
@@ -20,7 +20,7 @@ export class Enemy extends Phaser.Physics.Arcade.Sprite
     {   this.anims.create({
             key: 'BanditIdle',
             frames: this.anims.generateFrameNumbers('BanditIdle', { start: 0, end: 3 }),
-            frameRate: 10,
+            frameRate: 5,
             repeat: -1
         });
 
